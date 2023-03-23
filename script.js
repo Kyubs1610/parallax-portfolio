b313d13a9b08c296d3d61cd7f0e15ffff66f3acb
Kyubs1610 <diego.decort@gmail.com>
2023-03-23 12:34:37 +0100

    transformation

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
-document.getElementById("checkbox").onclick = function() {
-    myFunction()
-  };
-
+  // dark mode function
+  
   function myFunction() {
     let moon = document.querySelector(".moon");
     let sun = document.querySelector(".background");
     let body = document.querySelector("body");
+    let cursor = document.querySelector(".cursor");
+    let about = document.querySelector(".about");
+    let treenight = document.querySelector(".treenight");
+    let tree = document.querySelector(".tree");
+  
+      
     if (moon.style.display === 'block') {
       moon.style.display = 'none';
       sun.style.display = 'block';
       bird.style.display = 'block';
-      document.querySelector(".tree").style.filter = 'none';
+      tree.style.display = 'block';
+      treenight.style.display = 'none';
       body.style.backgroundColor = '#33100E';
       document.querySelector(".sec").style.backgroundColor = '#33100E';
       body.classList.remove("dark-mode");
+      about.addEventListener('mouseenter', () => {
+        cursor.style.display = 'none';
+      });
+      about.addEventListener('mouseleave', () => {
+        cursor.style.display = 'none';
+      });
     } else {
       sun.style.display = 'none';
       bird.style.display = 'none';
       moon.style.display = 'block';
-      document.querySelector(".tree").style.filter = 'grayscale(100%)';
-      body.style.backgroundColor = '#171717';
-      document.querySelector(".sec").style.backgroundColor = '#171717';
+      tree.style.display = 'none';
+      treenight.style.display = 'block';
+      body.style.backgroundColor = '#03000B';
+      document.querySelector(".sec").style.backgroundColor = '#03000B';
       body.classList.add("dark-mode");
+  
+      // Show cursor when hovering over the .about section
+      about.addEventListener('mouseenter', () => {
+        cursor.style.display = 'block';
+      });
+      about.addEventListener('mouseleave', () => {
+        cursor.style.display = 'none';
+      });
     }
   }
 
+  document.getElementById("checkbox").onclick = function() {
+    myFunction()
+  };
+  
+  // Scroll animation for the 1st section
   let texte = document.querySelector(".title");
   let tree = document.querySelector(".tree");
   let sun= document.querySelector(".background");
@@ -40,5 +65,41 @@ document.getElementById("checkbox").onclick = function() {
     texte.style.marginRight = value * 4 + 'px';
   })
 
-  ScrollReveal().reveal('.about', { delay: 600, duration: 1500 });
-  ScrollReveal().reveal('.fox', { delay: 600, duration: 1500 });
+ // Scroll reveal animation for the 2nd section 
+  ScrollReveal().reveal('.about h2',  { origin:'left',distance:'300px',delay: 600, duration: 1500 });
+  ScrollReveal().reveal('.work h2',  { origin:'left',distance:'300px',delay: 600, duration: 1500 });
+  ScrollReveal().reveal('.contactme h2',  { origin:'left',distance:'300px',delay: 600, duration: 1500 });
+  ScrollReveal().reveal('.about p', {origin:'right', distance:'300px',delay: 1000, duration: 1500 });
+  ScrollReveal().reveal('.container', { delay: 600, duration: 1500 });
+ 
+// Spot light effect on the dark mode
+  const cursor = document.querySelector('.cursor');
+  document.addEventListener('mousemove', e => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+  })
+
+// Work section   
+const portfolioModels = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+
+var portfolioIn = function (index) {
+    portfolioModels[index].classList.add("active");
+};
+
+imgCards.forEach((imgCard, i) => {
+     imgCard.addEventListener("click", () => {
+        portfolioIn(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModels.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
